52cf85a2669a254de774fdcf6b40bace69a284b1
DreamTeamSWE <95660158+DreamTeamSWE@users.noreply.github.com>
2022-02-10 18:35:32 +0100

    Update App.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,25 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import './App.css'
+import Card from './Components/Card';
+import Fetch from './Fetch';
 
-function Fetch() {
-    const [posts, setPosts] = useState([])
-    useEffect(() => {
-        axios.get('https://kg3afcrwig.execute-api.eu-central-1.amazonaws.com/PoC/db_crawling%27')
-            .then(res => {
-                console.log(res)
-                setPosts(res.data)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    })
-    return (
-        <div>
-            <ul>
-                {posts.map((post) => (
-                    <li key={post.id_post}>{post.id_utente}</li>
-                ))}
-            </ul>
-        </div>)
-}
-export default Fetch;
+
+function App() {
+  return (
+    <div className="card-class">
+      <Fetch />
+    </div>
+  )
+};
+
+export default App;
