08cee1639db26599e4ba3f5c6ef3b71a813071ca
DreamTeamSWE <95660158+DreamTeamSWE@users.noreply.github.com>
2022-02-10 17:44:52 +0100

    Update App.js

--- src/App/App.js
+++ src/App/App.js
@@ -3,37 +3,36 @@ import axios from 'axios';
 import './App.css'
 import Card from './Components/Card/Card';
 
-function Fetch() {
-    const [results, setPosts] = useState([])
-    useEffect(() => {
-        axios.get('https://kg3afcrwig.execute-api.eu-central-1.amazonaws.com/PoC/db_crawling%27')
-            .then(res => {
-                console.log(res)
-                setPosts(res.data)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    })
-    return (
-        <div className="card-class">
-                {results.map((value) => (
-                  <Card
-                      images3={value.image_s3}
-                      username={value.id_utente}
-                      post={value.testo_post}
-                      location={value.location}
-                      longitude={value.longitudine}
-                      latitude={value.latitudine}
-                      comprehend={value.sentiment}
-                      rekognition={value.tag_rekognition}
-                      phone={value.phone}
-                      website={value.web_site}
-                      category={value.category}
-                  />
-                ))}
-        </div>
-      )
+function App() {
+      const [results, setPosts] = useState([])
+      useEffect(() => {
+          axios.get('https://kg3afcrwig.execute-api.eu-central-1.amazonaws.com/PoC/db_crawling%27')
+              .then(res => {
+                  console.log(res)
+                  setPosts(res.data)
+              })
+              .catch(err => {
+                  console.log(err)
+              })
+      })
+      return (
+          <div className="card-class">
+                  {results.map((value) => (
+                    <Card
+                        images3={value.image_s3}
+                        username={value.id_utente}
+                        post={value.testo_post}
+                        location={value.location}
+                        longitude={value.longitudine}
+                        latitude={value.latitudine}
+                        comprehend={value.sentiment}
+                        rekognition={value.tag_rekognition}
+                        phone={value.phone}
+                        website={value.web_site}
+                        category={value.category}
+                    />
+                  ))}
+          </div>
+        )
   };
-  export default Fetch;
-
+  export default App;
