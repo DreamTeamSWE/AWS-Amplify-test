35abec5e172a04920cf370d3449239db9519ff27
DreamTeamSWE <95660158+DreamTeamSWE@users.noreply.github.com>
2022-02-10 18:20:50 +0100

    Update App.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,26 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './App.css'
-import Card from './Components/Card/Card';
 
-function App() {
-      const [posts, setPosts] = useState([])
-      useEffect(() => {
-          axios.get('https://kg3afcrwig.execute-api.eu-central-1.amazonaws.com/PoC/db_crawling%27')
-              .then(res => {
-                  console.log(res)
-                  setPosts(res.data)
-              })
-              .catch(err => {
-                  console.log(err)
-              })
-      })
-      return (
-          <div className="card-class">
-                  {posts.map((value) => (
-                   <li key={value.id_post}>{value.testo_post}</li>
-                  ))}
-          </div>
-        )
-  };
-  export default App;
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+function Fetch() {
+    const [posts, setPosts] = useState([])
+    useEffect(() => {
+        axios.get('https://kg3afcrwig.execute-api.eu-central-1.amazonaws.com/PoC/db_crawling%27')
+            .then(res => {
+                console.log(res)
+                setPosts(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    })
+    return (
+        <div>
+            <ul>
+                {posts.map((post) => (
+                    <li key={post.id_post}>{post.id_utente}</li>
+                ))}
+            </ul>
+        </div>)
+}
+export default Fetch;
